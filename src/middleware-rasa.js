c051cbccf07c2ef3a6cf2b891541c733bb00a2ff
Jose Luis Salazar Gonzalez <joseluis25sg@gmail.com>
2018-09-25 10:17:20 +0200

    Add ranking and translations

--- src/middleware-rasa.js
+++ src/middleware-rasa.js
@@ -15,18 +15,18 @@ module.exports = config => {
       // is_echo: can be true for facebook bots when the echo webhook is subscribed
       // bot_id: keep an eye https://github.com/howdyai/botkit/pull/694
       // if bot_id is present, the message comes from another bot
-      text_to_translate = (text_to_translate) ? text_to_translate : message.text;
-      if (!text_to_translate || message.is_echo || message.bot_id) {
+      const text_to_parse = (text_to_translate) ? text_to_translate : message.text;
+      if (!text_to_parse || message.is_echo || message.bot_id) {
         next()
         return
       }
 
-      debug('Sending message to Rasa', text_to_translate)
+      debug('Sending message to Rasa', text_to_parse)
       const options = {
         method: 'POST',
         uri: `${config.rasa_uri}/parse`,
         body: {
-          q: text_to_translate,
+          q: text_to_parse,
           project: `${config.rasa_project}`
         },
         json: true
@@ -34,10 +34,11 @@ module.exports = config => {
 
       request(options)
         .then(response => {
-          debug('Rasa response', response)
-          message.intent = response.intent
-          message.entities = response.entities
-          next()
+          debug('Rasa response', response);
+          message.intent = response.intent;
+          message.entities = response.entities;
+          message.intent_ranking = response.intent_ranking;
+          next();
         })
     },
 
